5149f76205ecd5821cf1dd58eb31b99b9be7a5eb
Able <abl3theabove@gmail.com>
2021-05-11 08:47:51 -0500

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
+#[macro_use]
+extern crate serde_derive;
+
 pub const PORT: &str = "hello";
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     pub id: String,
     pub name: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
     sender: User,
     text: String,
